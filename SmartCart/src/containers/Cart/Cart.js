33d8d4679abd83c3680a6ad2f8dd9d9648f24206
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-05-06 23:35:18 +0300

    Create Expo Prebuild

--- SmartCart/src/containers/Cart/Cart.js
+++ SmartCart/src/containers/Cart/Cart.js
@@ -1,11 +1,17 @@
-import { Button, StyleSheet, Text, View } from "react-native"
-import styles from "./style"
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import styles from "./style";
+import { torch } from "react-native-pytorch-core";
 
 export function CartContainer({ goToPaymentScreen }) {
+  const [tensor, _setTensor] = useState(torch.rand([2, 3]));
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>This is the cart page</Text>
+      <Text>{`Random tensor of shape ${
+        tensor.shape
+      } with data ${tensor.data()}`}</Text>
       <Button onPress={goToPaymentScreen} title="Purchase" />
     </View>
-  )
+  );
 }
