8dea78a222fc115aa256a2b44b6a510c1b0bf3fc
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-05-15 22:40:49 +0300

    New component: CircularIconButton

---
+++ SmartCart/src/components/CircularIconButton/CircularIconButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { TouchableOpacity, View, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export function CircularIconButton({ iconName, onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <View style={styles.iconContainer}>
+        <MaterialIcons name={iconName} size={24} color="white" />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
