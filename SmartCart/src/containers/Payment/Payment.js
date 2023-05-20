e8b0d8a37054c79ccf04e4af0c152f3a94f082ca
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-05-20 21:17:19 +0300

    Implement Payment Screen

--- SmartCart/src/containers/Payment/Payment.js
+++ SmartCart/src/containers/Payment/Payment.js
@@ -1,10 +1,117 @@
-import { StyleSheet, Text, View } from 'react-native';
-import styles from "./style"
+import {
+  SafeAreaView,
+  Alert,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import styles from "./style";
+import { GoBackButton } from "@Components/index";
+import {
+  TextInput,
+  Button,
+  Text,
+  Stack,
+  Divider,
+} from "@react-native-material/core";
 
-export function PaymentContainer() {
+export function PaymentContainer({
+  goBack,
+  totalAmount,
+  cardNumber,
+  setCardNumber,
+  expiryDate,
+  cvv,
+  setCvv,
+  isNumeric,
+  isValidExpiryDate,
+  handleChangeExpiryDate,
+}) {
+  const handleValidation = () => {
+    if (cardNumber.length !== 16 || !isNumeric(cardNumber)) {
+      Alert.alert(
+        "Invalid Card Number",
+        "Please enter a valid 16-digit card number."
+      );
+      return;
+    }
+
+    if (expiryDate.length !== 5 || !isValidExpiryDate(expiryDate)) {
+      Alert.alert(
+        "Invalid Expiry Date",
+        "Please enter a valid expiry date in the format MM/YY."
+      );
+      return;
+    }
+
+    if (cvv.length !== 3 || !isNumeric(cvv)) {
+      Alert.alert("Invalid CVV", "Please enter a valid 3-digit CVV.");
+      return;
+    }
+
+    Alert.alert(
+      "Validation Successful",
+      "Your credit card information is valid."
+    );
+  };
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>This is the payment page</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <SafeAreaView style={styles.container}>
+        <GoBackButton onPress={goBack} />
+        <Text variant="h3" style={styles.headerText}>
+          Make Payment
+        </Text>
+        <View style={styles.mainContainer}>
+          <Stack m={3} spacing={15}>
+            <TextInput
+              label="Card Number"
+              variant="outlined"
+              value={cardNumber}
+              onChangeText={setCardNumber}
+              keyboardType="numeric"
+              maxLength={16}
+              autoCapitalize="none"
+            />
+            <TextInput
+              label="Expiry Date (MM/YY)"
+              variant="outlined"
+              value={expiryDate}
+              onChangeText={handleChangeExpiryDate}
+              keyboardType="numeric"
+              maxLength={5}
+              autoCapitalize="none"
+            />
+            <TextInput
+              label="CVV"
+              variant="outlined"
+              value={cvv}
+              onChangeText={setCvv}
+              keyboardType="numeric"
+              maxLength={3}
+              autoCapitalize="none"
+            />
+          </Stack>
+          <View>
+            <Divider color="lightgrey" />
+            <View style={styles.totalContainer}>
+              <Text style={styles.totalText}>YOUR TOTAL</Text>
+              <Text style={styles.totalAmountText}>{totalAmount}TL</Text>
+            </View>
+
+            <Button
+              title="Purchase"
+              mode="contained"
+              onPress={handleValidation}
+              style={styles.button}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
--- SmartCart/src/screens/PaymentScreen/PaymentScreen.js
+++ SmartCart/src/screens/PaymentScreen/PaymentScreen.js
@@ -1,8 +1,72 @@
-import React from "react"
-import { PaymentContainer } from "../../containers"
+import React, { useState } from "react";
+import { PaymentContainer } from "../../containers";
+import { useNavigation } from "@react-navigation/native";
 
-export function PaymentScreen() {
+export function PaymentScreen(props) {
+  const navigation = useNavigation();
+
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  var totalAmount = [];
+
+  if (props.route.params && props.route.params.totalAmount) {
+    totalAmount = props.route.params.totalAmount;
+  } else {
+    totalAmount = [];
+  }
+
+  const isNumeric = (value) => {
+    return /^\d+$/.test(value);
+  };
+
+  const isValidExpiryDate = (value) => {
+    const [month, year] = value.split("/");
+    const currentYear = new Date().getFullYear() % 100;
+    const currentMonth = new Date().getMonth() + 1;
+
     return (
-        <PaymentContainer/>
-    )
+      isNumeric(month) &&
+      isNumeric(year) &&
+      parseInt(month) >= 1 &&
+      parseInt(month) <= 12 &&
+      parseInt(year) >= currentYear &&
+      !(parseInt(year) === currentYear && parseInt(month) < currentMonth)
+    );
+  };
+
+  const formatExpiryDate = (value) => {
+    let formattedValue = value.replace(/\//g, ""); // Remove existing slashes
+    if (formattedValue.length > 2) {
+      formattedValue =
+        formattedValue.slice(0, 2) + "/" + formattedValue.slice(2);
+    }
+    return formattedValue;
+  };
+
+  const handleChangeExpiryDate = (value) => {
+    const formattedValue = formatExpiryDate(value);
+    setExpiryDate(formattedValue);
+  };
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <PaymentContainer
+      goBack={goBack}
+      totalAmount={totalAmount}
+      cardNumber={cardNumber}
+      setCardNumber={setCardNumber}
+      expiryDate={expiryDate}
+      setExpiryDate={setExpiryDate}
+      cvv={cvv}
+      setCvv={setCvv}
+      isNumeric={isNumeric}
+      isValidExpiryDate={isValidExpiryDate}
+      handleChangeExpiryDate={handleChangeExpiryDate}
+    />
+  );
 }
